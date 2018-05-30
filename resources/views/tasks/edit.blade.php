50a41d080e10df1beb5b0c92f16cafaa258da6d2
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-05-30 06:34:35 +0000

    amend

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -7,7 +7,7 @@
 
     
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
         <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
@@ -18,7 +18,7 @@
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
 
-
+        </div>
         {!! Form::submit('更新', ['class' => 'btn btn-warning']) !!}
 
     {!! Form::close() !!}
