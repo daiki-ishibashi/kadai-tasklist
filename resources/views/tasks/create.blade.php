cff9711a45aeef3445edbddca85fe7b7d95b68ba
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-05-30 06:11:17 +0000

    amend

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,10 +4,10 @@
 
 <!-- Write content for each page here -->
     <h1>タスク新規作成ページ</h1>
-
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-        <div class= "form-group" , "col-xs-12" , "col-sm-offset-2 col-sm-8" , "col-lg-offset-3 col-lg-6">
+    
+        <div class="row"> 
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
                     {!! Form::label('status', 'ステータス:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
@@ -16,8 +16,7 @@
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
-                
-        
+        </div>
         
         <button type="button" class="btn btn-default">
           <span class="glyphicon glyphicon-sunglasses"></span>
