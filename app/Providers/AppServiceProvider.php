a6dd149f5404d8ab552b2a60ae942869ea08dd22
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-06-05 05:27:41 +0000

    tasklist

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,16 +13,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Schema::defaultStringLength(191);
     }
 
     /**
      * Register any application services.
-     *
      * @return void
      */
+     
+     
     public function register()
     {
-        \Schema::defaultStringLength(191);
+        //
     }
 }
